b603c239d4237dee1cf88f8f1b6ece5f419c41e4
lozshi <quangduy16072005@gmail.com>
2026-08-11 09:37:40 +0700

    fix(package): update brace-expansion and js-yaml versions in package-lock.json

--- eslint.config.js
+++ eslint.config.js
@@ -2,30 +2,33 @@ module.exports = [
   {
     languageOptions: {
       ecmaVersion: 2022,
-      sourceType: "commonjs",
+      sourceType: 'commonjs',
       globals: {
         // Node.js globals
-        process: "readonly",
-        console: "readonly",
-        require: "readonly",
-        module: "readonly",
-        __dirname: "readonly",
-        __filename: "readonly",
+        process: 'readonly',
+        console: 'readonly',
+        require: 'readonly',
+        module: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
         // Jest globals
-        jest: "readonly",
-        describe: "readonly",
-        test: "readonly",
-        it: "readonly",
-        expect: "readonly",
-        beforeEach: "readonly",
-        afterEach: "readonly",
-        beforeAll: "readonly",
-        afterAll: "readonly",
-      }
+        jest: 'readonly',
+        describe: 'readonly',
+        test: 'readonly',
+        it: 'readonly',
+        expect: 'readonly',
+        beforeEach: 'readonly',
+        afterEach: 'readonly',
+        beforeAll: 'readonly',
+        afterAll: 'readonly',
+      },
     },
     rules: {
-      "no-unused-vars": "warn",
-      "no-console": "off"
-    }
-  }
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+    },
+  },
+  {
+    ignores: ['coverage/**'],
+  },
 ];
